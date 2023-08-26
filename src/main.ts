9cf5d41797af19782e51b749bbe18124fe1c6bf9
JM Copino <jose.copino@vcdcph.com>
2023-08-26 22:40:55 +0800

    add prefix 'api'

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,9 @@ import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+
+  app.setGlobalPrefix('api');
+
   const configService = app.get(ConfigService);
   const port = configService.get('PORT');
 
