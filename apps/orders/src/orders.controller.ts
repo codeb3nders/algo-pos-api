b0fb1e8c79a8e6fc128479c9cf231c5d78665061
JM Copino <jose.copino@vcdcph.com>
2023-11-19 15:19:17 +0800

    -

--- apps/orders/src/orders.controller.ts
+++ apps/orders/src/orders.controller.ts
@@ -2,14 +2,15 @@ import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
 
 import { CreateOrderRequest } from './dto/create-order.request';
 import { OrdersService } from './orders.service';
-import { JwtAuthGuard } from '@app/shared';
+// import { JwtAuthGuard } from '@app/shared';
+import { JwtRabbitAuthGuard } from '@app/shared/auth/jwt-rabbit-auth.guard';
 
 @Controller('orders')
 export class OrdersController {
   constructor(private readonly ordersService: OrdersService) {}
 
   @Post()
-  @UseGuards(JwtAuthGuard)
+  @UseGuards(JwtRabbitAuthGuard)
   async createOrder(@Body() request: CreateOrderRequest, @Req() req: any) {
     return this.ordersService.createOrder(request, req.cookies?.Authentication);
   }
