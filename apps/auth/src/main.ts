5c4304ca1568b01292fd368b0bb4a33e303396f1
JM Copino <jose.copino@vcdcph.com>
2023-12-03 18:13:52 +0800

    get

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -1,6 +1,10 @@
 import { NestFactory } from '@nestjs/core';
-import * as cookieParser from 'cookie-parser';
+import { RmqService } from '@app/shared';
 import { AuthModule } from './auth.module';
+import { RmqOptions } from '@nestjs/microservices';
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AuthModule);
@@ -8,8 +12,13 @@ async function bootstrap() {
   app.enableCors({
     origin: ['http://localhost:3000', 'http://localhost:5173'],
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
   });
-  await app.listen(3001);
+
+  const rmqService = app.get<RmqService>(RmqService);
+  app.connectMicroservice<RmqOptions>(rmqService.getOptions('AUTH', true));
+  app.useGlobalPipes(new ValidationPipe());
+  const configService = app.get(ConfigService);
+  await app.startAllMicroservices();
+  await app.listen(configService.get('PORT'));
 }
 bootstrap();
--- apps/auth/src/users/users.controller.ts
+++ apps/auth/src/users/users.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
 import { CreateUserRequest } from './dto/create-user.request';
 import { UsersService } from './users.service';
 import { JwtAuthGuard } from '../guards/jwt-auth.guard';
+import { JwtGuard } from '../sub/guards';
 
 @Controller('auth/users')
 export class UsersController {
@@ -9,6 +10,7 @@ export class UsersController {
 
   @Get()
   // @UseGuards(JwtAuthGuard)
+  @UseGuards(JwtGuard)
   async getUsers(@Req() req) {
     console.log(req.cookie);
     return this.usersService.getAllUsers();
