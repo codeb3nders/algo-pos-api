1f60e39363bd23cd7608e8632391480c1548565b
JM <codeb3nder@gmail.com>
2023-12-31 08:46:56 +0800

    hitSlop

--- rn-ui/components/common/button-component.tsx
+++ rn-ui/components/common/button-component.tsx
@@ -4,6 +4,7 @@ import React from 'react';
 const ButtonComponent = ({ children, callback }: any) => {
   return (
     <TouchableOpacity
+      hitSlop={{ top: 20, left: 20, bottom: 20, right: 20 }}
       className="flex border border-gray-400 bg-algo-green-1 self-center  items-center rounded-full p-1 w-20 mt-5"
       onPress={callback}
     >
